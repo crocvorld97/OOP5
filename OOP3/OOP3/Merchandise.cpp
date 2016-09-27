9a77cd99c17df58edb5aa46ba3015beaf9893ce7
crocvorld97 <crocvorld_97@mail.ru>
2016-09-27 03:43:00 +0300

    firstDay

---
+++ OOP3/OOP3/Merchandise.cpp
@@ -0,0 +1,1 @@
+#include "Merchandise.h"
---
+++ OOP3/OOP3/OOP3.cpp
@@ -0,0 +1,16 @@
+// OOP3.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include "Cake.h"
+#include "Candy.h"
+#include "Flowers.h"
+#include "Merchandise.h"
+#include "Product.h"
+#include "Watch.h"
+
+int main()
+{
+    return 0;
+}
+
